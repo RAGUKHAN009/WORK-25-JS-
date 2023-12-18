72be2cd82799d3175b34de994cbefe06f005bc82
Aliyan Arif <143800651+RAGUKHAN009@users.noreply.github.com>
2023-12-18 21:54:51 +0500

    added new files

---
+++ script.js
@@ -0,0 +1,686 @@
+
+// hero filter
+
+
+function resetSelections() {
+
+    document.getElementById('select1').value = 'one1';
+
+    document.getElementById('select2').value = 'one2';
+
+    document.getElementById('select3').value = 'one3';
+
+    document.getElementById('select4').value = 'one4';
+
+    document.getElementById('select5').value = 'one5';
+
+
+}
+
+
+
+// AUDIO CONTROLS 
+
+
+
+// CART 1
+
+
+var audio1 = document.getElementById("myAudio1");
+
+function play1() {
+
+    // AUDIO WALAA LANJAAAAAA
+
+    if (audio1.paused) {
+        audio1.play();
+    } else {
+        audio1.pause();
+    }
+
+    // BUTTON KI APEARANCE KA LANJAAAA
+
+    var Z = document.getElementById('PAUSE1');
+    if (Z.style.display === 'block') {
+        Z.style.display = 'none';
+    } else {
+        Z.style.display = 'block';
+    }
+
+    var X = document.getElementById('PLAY1');
+    if (X.style.display === 'none') {
+        X.style.display = 'block';
+    } else {
+        X.style.display = 'none';
+    }
+}
+
+// cart 2
+
+
+var audio2 = document.getElementById("myAudio2");
+
+function play2() {
+
+    // AUDIO WALAA LANJAAAAAA
+
+    if (audio2.paused) {
+        audio2.play();
+    } else {
+        audio2.pause();
+    }
+
+    // BUTTON KI APEARANCE KA LANJAAAA
+
+    var Z = document.getElementById('PAUSE2');
+    if (Z.style.display === 'block') {
+        Z.style.display = 'none';
+    } else {
+        Z.style.display = 'block';
+    }
+
+    var X = document.getElementById('PLAY2');
+    if (X.style.display === 'none') {
+        X.style.display = 'block';
+    } else {
+        X.style.display = 'none';
+    }
+}
+
+
+// CART 3
+
+var audio3 = document.getElementById("myAudio3");
+
+function play3() {
+
+    // AUDIO WALAA LANJAAAAAA
+
+    if (audio3.paused) {
+        audio3.play();
+    } else {
+        audio3.pause();
+    }
+
+    // BUTTON KI APEARANCE KA LANJAAAA
+
+    var Z = document.getElementById('PAUSE3');
+    if (Z.style.display === 'block') {
+        Z.style.display = 'none';
+    } else {
+        Z.style.display = 'block';
+    }
+
+    var X = document.getElementById('PLAY3');
+    if (X.style.display === 'none') {
+        X.style.display = 'block';
+    } else {
+        X.style.display = 'none';
+    }
+}
+
+
+
+// CART 4
+
+var audio4 = document.getElementById("myAudio4");
+
+function play4() {
+
+    // AUDIO WALAA LANJAAAAAA
+
+    if (audio4.paused) {
+        audio4.play();
+    } else {
+        audio4.pause();
+    }
+
+    // BUTTON KI APEARANCE KA LANJAAAA
+
+    var Z = document.getElementById('PAUSE4');
+    if (Z.style.display === 'block') {
+        Z.style.display = 'none';
+    } else {
+        Z.style.display = 'block';
+    }
+
+    var X = document.getElementById('PLAY4');
+    if (X.style.display === 'none') {
+        X.style.display = 'block';
+    } else {
+        X.style.display = 'none';
+    }
+}
+
+
+
+// CART 5
+
+var audio5 = document.getElementById("myAudio5");
+
+function play5() {
+
+    // AUDIO WALAA LANJAAAAAA
+
+    if (audio5.paused) {
+        audio5.play();
+    } else {
+        audio5.pause();
+    }
+
+    // BUTTON KI APEARANCE KA LANJAAAA
+
+    var Z = document.getElementById('PAUSE5');
+    if (Z.style.display === 'block') {
+        Z.style.display = 'none';
+    } else {
+        Z.style.display = 'block';
+    }
+
+    var X = document.getElementById('PLAY5');
+    if (X.style.display === 'none') {
+        X.style.display = 'block';
+    } else {
+        X.style.display = 'none';
+    }
+}
+
+
+
+// CART 6
+
+var audio6 = document.getElementById("myAudio6");
+
+function play6() {
+
+    // AUDIO WALAA LANJAAAAAA
+
+    if (audio6.paused) {
+        audio6.play();
+    } else {
+        audio6.pause();
+    }
+
+    // BUTTON KI APEARANCE KA LANJAAAA
+
+    var Z = document.getElementById('PAUSE6');
+    if (Z.style.display === 'block') {
+        Z.style.display = 'none';
+    } else {
+        Z.style.display = 'block';
+    }
+
+    var X = document.getElementById('PLAY6');
+    if (X.style.display === 'none') {
+        X.style.display = 'block';
+    } else {
+        X.style.display = 'none';
+    }
+}
+8
+
+// CART 7
+
+var audio7 = document.getElementById("myAudio7");
+
+function play7() {
+
+    // AUDIO WALAA LANJAAAAAA
+
+    if (audio7.paused) {
+        audio7.play();
+    } else {
+        audio7.pause();
+    }
+
+    // BUTTON KI APEARANCE KA LANJAAAA
+
+    var Z = document.getElementById('PAUSE7');
+    if (Z.style.display === 'block') {
+        Z.style.display = 'none';
+    } else {
+        Z.style.display = 'block';
+    }
+
+    var X = document.getElementById('PLAY7');
+    if (X.style.display === 'none') {
+        X.style.display = 'block';
+    } else {
+        X.style.display = 'none';
+    }
+}
+
+
+
+// CART 8
+
+var audio8 = document.getElementById("myAudio8");
+
+function play8() {
+
+    // AUDIO WALAA LANJAAAAAA
+
+    if (audio8.paused) {
+        audio8.play();
+    } else {
+        audio8.pause();
+    }
+
+    // BUTTON KI APEARANCE KA LANJAAAA
+
+    var Z = document.getElementById('PAUSE8');
+    if (Z.style.display === 'block') {
+        Z.style.display = 'none';
+    } else {
+        Z.style.display = 'block';
+    }
+
+    var X = document.getElementById('PLAY8');
+    if (X.style.display === 'none') {
+        X.style.display = 'block';
+    } else {
+        X.style.display = 'none';
+    }
+}
+
+
+
+// CART 9
+
+var audio9 = document.getElementById("myAudio9");
+
+function play9() {
+
+    // AUDIO WALAA LANJAAAAAA
+
+    if (audio9.paused) {
+        audio9.play();
+    } else {
+        audio9.pause();
+    }
+
+    // BUTTON KI APEARANCE KA LANJAAAA
+
+    var Z = document.getElementById('PAUSE9');
+    if (Z.style.display === 'block') {
+        Z.style.display = 'none';
+    } else {
+        Z.style.display = 'block';
+    }
+
+    var X = document.getElementById('PLAY9');
+    if (X.style.display === 'none') {
+        X.style.display = 'block';
+    } else {
+        X.style.display = 'none';
+    }
+}
+
+
+
+// CART 10
+
+var audio10 = document.getElementById("myAudio10");
+
+function play10() {
+
+    // AUDIO WALAA LANJAAAAAA
+
+    if (audio10.paused) {
+        audio10.play();
+    } else {
+        audio10.pause();
+    }
+
+    // BUTTON KI APEARANCE KA LANJAAAA
+
+    var Z = document.getElementById('PAUSE10');
+    if (Z.style.display === 'block') {
+        Z.style.display = 'none';
+    } else {
+        Z.style.display = 'block';
+    }
+
+    var X = document.getElementById('PLAY10');
+    if (X.style.display === 'none') {
+        X.style.display = 'block';
+    } else {
+        X.style.display = 'none';
+    }
+}
+
+
+
+// CART 11
+
+var audio11 = document.getElementById("myAudio11");
+
+function play11() {
+
+    // AUDIO WALAA LANJAAAAAA
+
+    if (audio11.paused) {
+        audio11.play();
+    } else {
+        audio11.pause();
+    }
+
+    // BUTTON KI APEARANCE KA LANJAAAA
+
+    var Z = document.getElementById('PAUSE11');
+    if (Z.style.display === 'block') {
+        Z.style.display = 'none';
+    } else {
+        Z.style.display = 'block';
+    }
+
+    var X = document.getElementById('PLAY11');
+    if (X.style.display === 'none') {
+        X.style.display = 'block';
+    } else {
+        X.style.display = 'none';
+    }
+}
+
+
+
+// CART 12
+
+var audio12 = document.getElementById("myAudio12");
+
+function play12() {
+
+    // AUDIO WALAA LANJAAAAAA
+
+    if (audio12.paused) {
+        audio12.play();
+    } else {
+        audio12.pause();
+    }
+
+    // BUTTON KI APEARANCE KA LANJAAAA
+
+    var Z = document.getElementById('PAUSE12');
+    if (Z.style.display === 'block') {
+        Z.style.display = 'none';
+    } else {
+        Z.style.display = 'block';
+    }
+
+    var X = document.getElementById('PLAY12');
+    if (X.style.display === 'none') {
+        X.style.display = 'block';
+    } else {
+        X.style.display = 'none';
+    }
+}
+
+
+
+// LIKE 
+
+// CART 1
+
+
+function hide1() {
+
+
+    var z = document.getElementById('show1');
+    if (z.style.display === 'block') {
+        z.style.display = 'none';
+    } else {
+        z.style.display = 'block';
+    }
+
+    var x = document.getElementById('hide1');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+// cart 2 
+
+function hide2() {
+
+
+    var z = document.getElementById('show2');
+    if (z.style.display === 'block') {
+        z.style.display = 'none';
+    } else {
+        z.style.display = 'block';
+    }
+
+    var x = document.getElementById('hide2');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+
+// cart 3
+
+function hide3() {
+
+
+    var z = document.getElementById('show3');
+    if (z.style.display === 'block') {
+        z.style.display = 'none';
+    } else {
+        z.style.display = 'block';
+    }
+
+    var x = document.getElementById('hide3');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+
+
+// cart 4
+
+function hide4() {
+
+
+    var z = document.getElementById('show4');
+    if (z.style.display === 'block') {
+        z.style.display = 'none';
+    } else {
+        z.style.display = 'block';
+    }
+
+    var x = document.getElementById('hide4');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+
+
+// cart 5
+
+function hide5() {
+
+
+    var z = document.getElementById('show5');
+    if (z.style.display === 'block') {
+        z.style.display = 'none';
+    } else {
+        z.style.display = 'block';
+    }
+
+    var x = document.getElementById('hide5');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+
+
+// cart 6
+
+function hide6() {
+
+
+    var z = document.getElementById('show6');
+    if (z.style.display === 'block') {
+        z.style.display = 'none';
+    } else {
+        z.style.display = 'block';
+    }
+
+    var x = document.getElementById('hide6');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+
+
+// cart 7
+
+function hide7() {
+
+
+    var z = document.getElementById('show7');
+    if (z.style.display === 'block') {
+        z.style.display = 'none';
+    } else {
+        z.style.display = 'block';
+    }
+
+    var x = document.getElementById('hide7');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+
+
+// cart 8
+
+function hide8() {
+
+
+    var z = document.getElementById('show8');
+    if (z.style.display === 'block') {
+        z.style.display = 'none';
+    } else {
+        z.style.display = 'block';
+    }
+
+    var x = document.getElementById('hide8');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+
+
+// cart 9
+
+function hide9() {
+
+
+    var z = document.getElementById('show9');
+    if (z.style.display === 'block') {
+        z.style.display = 'none';
+    } else {
+        z.style.display = 'block';
+    }
+
+    var x = document.getElementById('hide9');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+
+
+// cart 10
+
+function hide10() {
+
+
+    var z = document.getElementById('show10');
+    if (z.style.display === 'block') {
+        z.style.display = 'none';
+    } else {
+        z.style.display = 'block';
+    }
+
+    var x = document.getElementById('hide10');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+
+
+// cart 11
+
+function hide11() {
+
+
+    var z = document.getElementById('show11');
+    if (z.style.display === 'block') {
+        z.style.display = 'none';
+    } else {
+        z.style.display = 'block';
+    }
+
+    var x = document.getElementById('hide11');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+
+
+// cart 12
+
+function hide12() {
+
+
+    var z = document.getElementById('show12');
+    if (z.style.display === 'block') {
+        z.style.display = 'none';
+    } else {
+        z.style.display = 'block';
+    }
+
+    var x = document.getElementById('hide12');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+
+
+
+
+
